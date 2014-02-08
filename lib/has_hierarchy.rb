2d94eaed49f5bbede4a2ef35ba121dc7535154b1
Kolesnikov Danil <kolesnikovde@gmail.com>
2014-02-08 16:58:46 +1000

    Added #set_default_position? support.

--- lib/has_hierarchy.rb
+++ lib/has_hierarchy.rb
@@ -8,20 +8,35 @@ module HasHierarchy
     has_order options.merge(scope: :parent_id)
     has_children options
 
+    after_save :reset_parent_acceptance
+
     include InstanceMethods
   end
 
   module InstanceMethods
     def move_before node
-      self.parent_id = node.parent_id
-
+      accept_parent(node)
       super
     end
 
     def move_after node
-      self.parent_id = node.parent_id
-
+      accept_parent(node)
       super
+    end
+
+    protected
+
+    def accept_parent node
+      self.parent_id = node.parent_id
+      @parent_accepted = true
+    end
+
+    def reset_parent_acceptance
+      @parent_accepted = false
+    end
+
+    def set_default_position?
+      super or parent_id_changed? and not @parent_accepted
     end
   end
 end
--- spec/db/schema.rb
+++ spec/db/schema.rb
@@ -2,7 +2,7 @@ ActiveRecord::Schema.define(version: 0) do
   create_table :items, force: true do |t|
     t.string :name
     t.string :node_path
-    t.integer :position, default: 0
+    t.integer :position
 
     t.belongs_to :parent
   end
